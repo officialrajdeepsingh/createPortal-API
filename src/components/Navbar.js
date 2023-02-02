10e810151416a4f836a5674cbd98c23ea445fb6f
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-02 14:43:28 +0530

    create the navbar components

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar bg-primary mb-4">
+        <div class="container-fluid" style={{justifyContent:"center"}}>
+            <a class="navbar-brand text-center" href="/">
+            createPortal API demo
+            </a>
+        </div>
+        </nav>
+    );
+}
+
+export default Navbar;
